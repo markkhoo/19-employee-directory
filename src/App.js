37cd45f8b06f9243f8509851279b3d3dce05105d
markeezy <markkhoo95@gmail.com>
2021-05-21 19:47:13 -0700

    add the stuff in App plus and create row

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import axios from "axios";
+import React from "react";
+import { useState, useReducer, useEffect } from 'react'
+import TableRow from "./components/row";
+
 import './App.css';
 
 function App() {
+
+
   return (
-    <div>
+    <div className="App">
       <header className="App-header">
         <h1>Employee Directory</h1>
       </header>
-
+      <div className="Search">
+        <input type="text"></input>
+      </div>
+      <table>
+        <tr>
+          <th>Image</th>
+          <th>Name</th>
+          <th>Phone</th>
+          <th>Email</th>
+          <th>DOB</th>
+        </tr>
+      </table>
     </div>
   );
 }
---
+++ src/components/row.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function TableRow(props) {
+    return (
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    );
+};
+
+export default TableRow;
